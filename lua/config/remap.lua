a02a41f7ee5b41ef092a6ab3bb12b28d4e0d86f5
Ben <smithbenjamin@ufl.edu>
2024-01-20 12:52:26 -0500

    init commit

---
+++ lua/config/remap.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("i", "(", "()<Esc>i")
+vim.keymap.set("i", "[", "[]<Esc>i")
+vim.keymap.set("i", "{", "{}<Esc>i")
+vim.keymap.set("i", "'", "''<Esc>i")
+vim.keymap.set("i", '"', '""<Esc>i')
+
